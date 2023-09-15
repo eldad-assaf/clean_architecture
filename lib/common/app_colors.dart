76f0233d0a8f6631c8582bd893918469e39d40c8
eldad-assaf <eldadux@gmail.com>
2023-09-15 14:21:55 +0300

    folders and data data_state

--- lib/common/app_colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-	static final Color baseColor = Color(0xFF212735);
-}
--- lib/common/app_routes.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppRoutes {
-  static const String root = "/";
-}
